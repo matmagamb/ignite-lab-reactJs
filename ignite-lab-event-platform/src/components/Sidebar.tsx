dd0a3315caeef9a94fa1956da214421de96cd7cb
matmagamb <ceifa13@outlook.com>
2022-06-26 13:28:02 -0300

    aula 5

--- ignite-lab-event-platform/src/components/Sidebar.tsx
+++ ignite-lab-event-platform/src/components/Sidebar.tsx
@@ -1,30 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-query {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}`;
-
-interface GetLessonsQUeryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string
-    lessonType: 'live' | 'class'
-
-  }[]
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQUeryResponse>(GET_LESSONS_QUERY)
+  const { data } = useGetLessonsQuery()
   //console.log(data)
 
   return (
